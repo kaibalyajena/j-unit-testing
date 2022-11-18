cfb758839fc90c6f4665fff9b169ff6461794344
kaibalya jena <kaibalya18j@gmail.com>
2022-11-18 13:59:19 +0530

    j unit functions file

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,10 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public int addNumbers(int a, int b) {
+		return a + b;
+	}
+	public String addStrings(String str1, String str2) {
+		return str1 + str2;
+	}
+}
